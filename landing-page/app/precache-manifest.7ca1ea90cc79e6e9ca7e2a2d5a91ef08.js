30df95c24d29d07be51e5a5b57aea4b1b4daa48b
jul001 <juchen101@gmail.com>
2019-10-13 10:17:53 -0700

    updated website and react element

---
+++ landing-page/app/precache-manifest.7ca1ea90cc79e6e9ca7e2a2d5a91ef08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7db3d02d71323e914b52c75fd3f77d0d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ed70f0e7052d86a73704",
+    "url": "./static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "eb70a35aa0c09147d878",
+    "url": "./static/css/main.627fc387.chunk.css"
+  },
+  {
+    "revision": "ed70f0e7052d86a73704",
+    "url": "./static/js/2.6bde7bcb.chunk.js"
+  },
+  {
+    "revision": "eb70a35aa0c09147d878",
+    "url": "./static/js/main.4821aab6.chunk.js"
+  },
+  {
+    "revision": "e1071a85e11d40bc5a54",
+    "url": "./static/js/runtime-main.894fea0f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "./static/media/logo.25bf045c.svg"
+  }
+]);
