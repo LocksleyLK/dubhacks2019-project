f393e868d305837fbc1e0a85ad013716440208e0
mckaulay <mckaulay@gmail.com>
2019-10-13 11:27:51 -0700

    Add buid

---
+++ landing-page/app/precache-manifest.f3dd8712ff3ecf89d4e5cb0666ee4cdd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "440ede3ab5a5aec406e438debcee221c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "67d93cd6c9abb92bfa5a",
+    "url": "./static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "830070d8d283c1abd38a",
+    "url": "./static/css/main.ec13b8d1.chunk.css"
+  },
+  {
+    "revision": "67d93cd6c9abb92bfa5a",
+    "url": "./static/js/2.571ff69b.chunk.js"
+  },
+  {
+    "revision": "830070d8d283c1abd38a",
+    "url": "./static/js/main.401da38b.chunk.js"
+  },
+  {
+    "revision": "e1071a85e11d40bc5a54",
+    "url": "./static/js/runtime-main.894fea0f.js"
+  },
+  {
+    "revision": "7354aa40db8dacc8e5995f91902015cc",
+    "url": "./static/media/transparantColoredLogo.7354aa40.png"
+  }
+]);
