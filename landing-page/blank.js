3ad4a4f0ca2d5bbd167a56f73d643a9d4f95609b
jul001 <juchen101@gmail.com>
2019-10-13 09:07:03 -0700

    new layout

---
+++ landing-page/blank.js
@@ -0,0 +1,10 @@
+alert('Welcome');
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
