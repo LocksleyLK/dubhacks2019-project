2321f58def91f0a9d103d9074218ac96f008b6f0
mckaulay <mckaulay@gmail.com>
2019-10-13 11:22:19 -0700

    Add gallery

--- application/src/App.js
+++ application/src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
+import logo from "./transparantColoredLogo.png";
 import "./App.css";
 
 import { Button, Breadcrumb, Layout, Menu } from "antd";
+
+import Gallery from "./Gallery";
 
 import withFirebaseAuth from "react-with-firebase-auth";
 import * as firebase from "firebase/app";
@@ -18,6 +20,17 @@ const providers = {
 
 const { Header, Content, Footer } = Layout;
 
+let places = [
+  { title: "House in Ballard", path: "./images/Ballard.jpg" },
+  { title: "House in Magnolia", path: "./images/Magnolia.jpg" },
+  { path: "./images/North.jpg" },
+  { path: "./images/OurBishopLakeUnion.jpg" },
+  { path: "./images/OurLadyEast.jpg" },
+  { path: "./images/Southwest.jpg" },
+  { path: "./images/StGeorgeRainierValley.jpg" },
+  { path: "./images/Wallingford.png" }
+];
+
 class App extends React.Component {
   render() {
     const { user, signOut, signInWithGoogle } = this.props;
@@ -29,40 +42,41 @@ class App extends React.Component {
     // }
 
     return (
-      // <Layout>
-      //   <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
-      //     <div className="logo" />
-      //     <Menu
-      //       theme="dark"
-      //       mode="horizontal"
-      //       // defaultSelectedKeys={["2"]}
-      //       style={{ lineHeight: "64px" }}
-      //     >
-      //       <Menu.Item key="1">nav 1</Menu.Item>
-      //       <Menu.Item key="2">nav 2</Menu.Item>
-      //       <Menu.Item key="3">nav 3</Menu.Item>
-      //     </Menu>
-      //   </Header>
-      //   <Content style={{ padding: "0 50px", marginTop: 64 }}>
-      //     <Breadcrumb style={{ margin: "16px 0" }}>
-      //       <Breadcrumb.Item>Home</Breadcrumb.Item>
-      //       <Breadcrumb.Item>List</Breadcrumb.Item>
-      //       <Breadcrumb.Item>App</Breadcrumb.Item>
-      //     </Breadcrumb>
-      //     <div style={{ background: "#fff", padding: 24, minHeight: 380 }}>Content</div>
-      //   </Content>
-      //   <Footer style={{ textAlign: "center" }}>Ant Design ©2018 Created by Ant UED</Footer>
-      // </Layout>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          {user ? <p>Hello, {user.displayName}</p> : <p>Please sign in.</p>}
-          {user ? (
-            <Button onClick={signOut}>Sign out</Button>
-          ) : (
-            <Button onClick={signInWithGoogle}>Sign in with Google</Button>
-          )}
-        </header>
+        <Layout>
+          <Header style={{ position: "fixed", zIndex: 1, width: "100%", textAlign: "right" }}>
+            {/* <div className="logo" /> */}
+            {/* <Menu
+            theme="dark"
+            mode="horizontal"
+            // defaultSelectedKeys={["2"]}
+            style={{ lineHeight: "64px" }}
+          >
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu> */}
+            <span style={{ color: "#ffffff", marginRight: 12 }}>
+              {user ? `Hello, ${user.displayName}` : `Please sign in.`}
+            </span>
+            {user ? (
+              // <div>
+              <Button onClick={signOut}>Sign out</Button>
+            ) : (
+              // </div>
+              <Button onClick={signInWithGoogle}>Sign in with Google</Button>
+            )}
+          </Header>
+          <Content style={{ padding: "0 50px", marginTop: 64 }}>
+            {/* <header className="App-header"> */}
+            <img src={logo} className="App-logo" alt="logo" />
+            {/* </header> */}
+            <div style={{ background: "#fff", padding: 24, minHeight: 380 }}>
+              {user ? <Gallery data={places} /> : <strong>Please sign in.</strong>}
+            </div>
+          </Content>
+          <Footer style={{ textAlign: "center" }}>Created at Dubhacks 2019</Footer>
+        </Layout>
       </div>
     );
   }
